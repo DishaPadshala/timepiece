fb641c6df1fa0f769b1a6703d09e7b798e7f3411
dishapadshala <dishapadshala20@gmail.com>
2026-05-22 19:17:17 -0400

    Fix branch and repo name in actions

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
-const repositoryName = process.env.GITHUB_REPOSITORY?.split('/')[1];
-
 export default defineConfig({
-  base: repositoryName ? `/${repositoryName}/` : './',
+  base: '/timepiece/',
   plugins: [react()],
   build: {
     emptyOutDir: false,
